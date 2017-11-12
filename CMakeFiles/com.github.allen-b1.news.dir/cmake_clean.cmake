99a1e17eb3c6ad1e81aa49e15a94af3c138298ed
Allen B <pingguo.ny@gmail.com>
2017-11-12 11:17:47 -0500

    add cmakefiles

---
+++ CMakeFiles/com.github.allen-b1.news.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "src/news.c"
+  "src/NewsAboutDialog.c"
+  "src/NewsHeaderBar.c"
+  "src/NewsList.c"
+  "src/NewsPanel.c"
+  "src/NewsParse.c"
+  "src/widgets.c"
+  "com.github.allen-b1.news_valac.stamp"
+  "CMakeFiles/com.github.allen-b1.news.dir/src/news.c.o"
+  "CMakeFiles/com.github.allen-b1.news.dir/src/NewsAboutDialog.c.o"
+  "CMakeFiles/com.github.allen-b1.news.dir/src/NewsHeaderBar.c.o"
+  "CMakeFiles/com.github.allen-b1.news.dir/src/NewsList.c.o"
+  "CMakeFiles/com.github.allen-b1.news.dir/src/NewsPanel.c.o"
+  "CMakeFiles/com.github.allen-b1.news.dir/src/NewsParse.c.o"
+  "CMakeFiles/com.github.allen-b1.news.dir/src/widgets.c.o"
+  "com.github.allen-b1.news.pdb"
+  "com.github.allen-b1.news"
+  "com.github.allen-b1.news.pdb"
+  "CMakeFiles/CMakeRelink.dir/com.github.allen-b1.news"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/com.github.allen-b1.news.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
